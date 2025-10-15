e0ea6d3c0894334d4043a6cccdb60a6d771709ee
Hua Hua Cao <huahuacao.hh@usp.br>
2025-10-15 01:40:00 -0300

    add Home and modified main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+import Home from './Home.tsx'
+import Members from "./Members.tsx"
+import Resources from "./Resources.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/members" element={<Members />}/>
+        <Route path="/resources" element={<Resources />}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
+
