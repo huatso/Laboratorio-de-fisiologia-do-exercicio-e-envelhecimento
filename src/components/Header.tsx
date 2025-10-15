56d38048f1a6c3b6644de3b5092ffdfb60865ff9
Wu <wukamlong@usp.br>
2025-10-15 02:19:25 -0300

    Updated Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,34 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// 1. Import the Link component
+import { Link } from 'react-router-dom';
+
+// Import your custom CSS
+import './Header.css';
 
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar expand="lg" className="custom-navbar" sticky="top" variant="dark">
+      <Container fluid className="px-4">
+        {/* 2. Update Navbar.Brand */}
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src="/logo.png"
+            alt="LAFEE Logo"
+            className="d-inline-block align-top logo-img"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="ms-auto">
+            {/* 3. Update all Nav.Link components */}
+            <Nav.Link as={Link} to="/">Início</Nav.Link>
+            <Nav.Link as={Link} to="/research">Pesquisa</Nav.Link>
+            <Nav.Link as={Link} to="/publication">Publicações</Nav.Link>
+            <Nav.Link as={Link} to="/members">Equipe</Nav.Link>
+            <Nav.Link as={Link} to="/resources">Recursos</Nav.Link>
+            <Nav.Link as={Link} to="/about">Sobre</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
