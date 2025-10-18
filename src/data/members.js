0b9bcf92d6ef3824fb710f612e9f9a7453cbe6bb
Hua Hua Cao <huahuacao.hh@usp.br>
2025-10-18 01:23:02 -0300

    added samples

---
+++ src/data/members.js
@@ -0,0 +1,16 @@
+import DefaultProfile from "../assets/profile/Default"
+
+const MembersData = [
+    {
+        Id: 1,
+        Name: 1,
+        ImageUrl: DefaultProfile,
+        Teaser: "",
+        Bio: "",
+        Location: "",
+        ContactLinks: "", //Email, Linkedin, Github, etc
+    },
+];
+
+export default MembersData;
+
